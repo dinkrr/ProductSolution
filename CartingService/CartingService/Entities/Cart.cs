506c0a73fec193813c1b2bc63c3f518c0bae0156
Suraj Kentura <Suraj_Kentura@epam.com>
2024-08-28 12:26:34 +0530

    some changes

--- CartingService/CartingService/Entities/Cart.cs
+++ CartingService/CartingService/Entities/Cart.cs
@@ -5,6 +5,7 @@ namespace CartingService.Entities
     public class Cart
     {
         public string Id { get; set; }
+        public string SomeProperty { get; set; }
         public List<CartItem> Items { get; set; }
     }
 }
